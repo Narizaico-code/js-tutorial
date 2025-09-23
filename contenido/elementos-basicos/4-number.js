9987b5282d03a672a15f29a720c02fb123b5e62b
Angel Reyes <areyes-2024435@kinal.edu.gt>
2025-09-23 15:35:46 -0600

    002 - agregados elementos basicos js DK

---
+++ contenido/elementos-basicos/4-number.js
@@ -0,0 +1,25 @@
+/*
+        Tipo de dato number `NUMBER`
+*/
+// 1. entero
+    const enter = 33
+    const decimal = 3.34
+// 2. notacion cientifica
+    const cientifica = 5e4
+// 3. infinito y NaN(No es un Numero, Not a Number)
+    const infinito = Infinity
+    const noEsNumero = NaN
+// Operaciones aritmeticas
+// 1. suma, resta, multiplicacion y division
+    const suma = 5 + 6
+    const resta = 10 - 10
+    const multiplicacion = 3*4
+    const division = 16/4
+// 2. modulo y exponenciacion
+    const modulo = 16 % 8 // El resultado es 0
+    const exponenciacion = 2**4
+
+// La presicion en JavaScript
+    const resultado = 0.1 + 0.2 // Da 0.3 (En teoría).
+    // console.log(resultado.toFixed(1) == 0.3) // 
+    // console.log(resultado.toFixed(1))
